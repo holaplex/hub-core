3497cb5c1d69e91e97ba92b641291aa9e66cb8f3
raykast <me@ryan-s.net>
2023-01-04 11:16:47 -0800

    Add tentative Loki exporter support.

--- crates/core/src/lib.rs
+++ crates/core/src/lib.rs
@@ -10,10 +10,12 @@
 #![warn(clippy::pedantic, clippy::cargo, missing_docs)]
 
 pub extern crate anyhow;
+pub extern crate async_trait;
 pub extern crate clap;
 pub extern crate futures_util;
-#[cfg(feature = "rt")]
 pub extern crate tokio;
+pub extern crate tracing;
+pub extern crate url;
 
 pub use runtime::*;
 
@@ -32,10 +34,11 @@ pub mod prelude {
     pub use async_trait::async_trait;
     pub use futures_util::{FutureExt, StreamExt, TryFutureExt};
     pub use tracing::{
-        debug, debug_span, error, error_span, info, info_span, instrument, trace, trace_span, warn,
-        warn_span,
+        self, debug, debug_span, error, error_span, info, info_span, instrument, trace, trace_span,
+        warn, warn_span,
     };
     pub use tracing_subscriber::prelude::*;
+    pub use url::Url;
 
     /// Result helper that defaults to [`anyhow::Error`]
     pub type Result<T, E = Error> = std::result::Result<T, E>;
@@ -47,20 +50,19 @@ mod runtime {
         path::{Path, PathBuf},
     };
 
-    use tracing_subscriber::EnvFilter;
+    use tracing_subscriber::{layer::Layered, EnvFilter};
 
     use crate::prelude::*;
 
     #[derive(Debug, clap::Args)]
     struct CommonArgs<T: clap::Args> {
         /// The capacity of the async thread pool
-        #[cfg(feature = "rt")]
-        #[clap(short, env)]
+        #[arg(short, env)]
         jobs: Option<usize>,
 
         /// The Kafka broker list
         #[cfg(feature = "kafka")]
-        #[clap(long, env)]
+        #[arg(long, env)]
         kafka_brokers: String,
 
         #[command(flatten)]
@@ -83,7 +85,6 @@ mod runtime {
     #[non_exhaustive]
     #[derive(Debug)]
     pub struct Common {
-        #[cfg(feature = "rt")]
         rt: tokio::runtime::Runtime,
 
         #[cfg(feature = "kafka")]
@@ -93,26 +94,33 @@ mod runtime {
     }
 
     impl Common {
-        #[instrument(name = "init_runtime")]
-        fn new<T: fmt::Debug + clap::Args>(args: CommonArgs<T>) -> Result<(Self, T)> {
+        #[instrument(name = "init_runtime", skip(loki_task))]
+        fn new<T: fmt::Debug + clap::Args>(
+            args: CommonArgs<T>,
+            loki_task: Option<tracing_loki::BackgroundTask>,
+        ) -> Result<(Self, T)> {
             let CommonArgs {
-                #[cfg(feature = "rt")]
                 jobs,
                 #[cfg(feature = "kafka")]
                 kafka_brokers,
                 extra,
             } = args;
 
-            #[cfg(feature = "rt")]
             let jobs = jobs.unwrap_or_else(num_cpus::get);
 
-            #[cfg(feature = "rt")]
             let rt = tokio::runtime::Builder::new_multi_thread()
                 .enable_all()
                 .worker_threads(jobs)
                 .max_blocking_threads(jobs)
                 .build()
                 .context("Failed to construct Tokio runtime")?;
+
+            if let Some(loki_task) = loki_task {
+                rt.spawn(async move {
+                    loki_task.await;
+                    error!("Loki exporter task quit unexpectedly!");
+                });
+            }
 
             #[cfg(feature = "kafka")]
             let (producer, consumer) = rt.block_on(async {
@@ -155,7 +163,6 @@ mod runtime {
 
             Ok((
                 Self {
-                    #[cfg(feature = "rt")]
                     rt,
                     #[cfg(feature = "kafka")]
                     producer,
@@ -167,7 +174,6 @@ mod runtime {
         }
 
         /// Expose the Tokio async runtime
-        #[cfg(feature = "tokio")]
         pub fn rt(&self) -> &tokio::runtime::Runtime {
             &self.rt
         }
@@ -187,68 +193,130 @@ mod runtime {
         #[arg(long, env = "RUST_LOG")]
         log_filter: Option<String>,
 
+        /// Endpoint to use for exporting logs to Grafana Loki
+        #[arg(long, env)]
+        loki_endpoint: Option<Url>,
+
         #[command(flatten)]
         common: CommonArgs<T>,
     }
 
-    /// Perform environment setup and run the requested entrypoint
-    #[instrument(skip(main))]
-    pub fn run<T: fmt::Debug + clap::Args>(main: impl FnOnce(Common, T) -> Result<(), ()>) {
-        [
-            ".env.local",
-            if cfg!(debug_assertions) {
-                ".env.dev"
-            } else {
-                ".env.prod"
-            },
-            ".env",
-        ]
-        .into_iter()
-        .try_for_each(|p| {
-            dotenv(p)
-                .map(|_| ())
-                .with_context(|| format!("Failed to load .env file {p:?}"))
+    macro_rules! init_error {
+        ($($args:tt)*) => ({
+            ::tracing::error!($($args)*);
+            ::std::process::exit(1);
         })
-        .expect("Failed to load .env files");
+    }
 
-        let Opts { log_filter, common } = clap::Parser::parse();
+    fn fmt_layer<S>() -> tracing_subscriber::fmt::Layer<S> {
+        tracing_subscriber::fmt::layer()
+    }
 
-        let log_filter = log_filter.map_or_else(
-            || {
-                Borrowed(if cfg!(debug_assertions) {
-                    "debug"
-                } else {
-                    "info"
-                })
-            },
-            Owned,
+    #[instrument(name = "bootstrap_logger", skip(log_filter, f))]
+    fn init_subscriber<
+        S: tracing::Subscriber + for<'a> tracing_subscriber::registry::LookupSpan<'a>,
+    >(
+        log_filter: impl AsRef<str>,
+        f: impl FnOnce(Layered<EnvFilter, tracing_subscriber::Registry>) -> S,
+    ) where
+        Layered<tracing_subscriber::fmt::Layer<S>, S>: Into<tracing::Dispatch>,
+    {
+        let log_filter = log_filter.as_ref();
+        let reg = tracing_subscriber::Registry::default().with(
+            EnvFilter::try_new(log_filter).unwrap_or_else(|e| {
+                init_error!("Invalid log filter {log_filter:?}: {e}");
+            }),
         );
 
-        tracing_log::LogTracer::init().expect("Failed to initialize LogTracer");
-        tracing::subscriber::set_global_default(
-            tracing_subscriber::Registry::default()
-                .with(EnvFilter::try_new(&log_filter).unwrap_or_else(|e| {
-                    eprintln!("Invalid log filter {log_filter:?}: {e}");
-                    std::process::exit(1);
-                }))
-                .with(tracing_subscriber::fmt::layer()),
-        )
-        .expect("Failed to set default tracing subscriber");
+        f(reg)
+            .with(fmt_layer())
+            .try_init()
+            .unwrap_or_else(|e| init_error!("Failed to set tracing subscriber: {e}"));
+    }
 
-        let (common, extra) = match Common::new(common) {
-            Ok(t) => t,
-            Err(e) => {
-                error!("Failed to initialize runtime: {e:?}");
-                std::process::exit(-1);
-            },
-        };
+    /// Perform environment setup and run the requested entrypoint
+    pub fn run<T: fmt::Debug + clap::Args>(main: impl FnOnce(Common, T) -> Result<(), ()>) {
+        // Construct a temporary logger on this thread until the full logger is ready
+        let smuggled = tracing::subscriber::with_default(
+            tracing_subscriber::Registry::default().with(fmt_layer()),
+            || {
+                let span = error_span!("boot").entered();
 
-        std::process::exit(match main(common, extra) {
-            Ok(()) => 0,
-            Err(e) => {
-                error!("{e:?}");
-                1
+                [
+                    ".env.local",
+                    if cfg!(debug_assertions) {
+                        ".env.dev"
+                    } else {
+                        ".env.prod"
+                    },
+                    ".env",
+                ]
+                .into_iter()
+                .try_for_each(|p| {
+                    dotenv(p)
+                        .map(|_| ())
+                        .with_context(|| format!("Failed to load .env file {p:?}"))
+                })
+                .unwrap_or_else(|e| init_error!("Failed to load .env files: {e:?}"));
+
+                let opts = clap::Parser::parse();
+                std::mem::drop(span);
+                let span = error_span!("boot", ?opts).entered();
+                let Opts {
+                    log_filter,
+                    loki_endpoint,
+                    common,
+                } = opts;
+
+                let log_filter = log_filter.map_or_else(
+                    || {
+                        Borrowed(if cfg!(debug_assertions) {
+                            "debug"
+                        } else {
+                            "info"
+                        })
+                    },
+                    Owned,
+                );
+
+                let (loki_layer, loki_task) = loki_endpoint
+                    .map(|e| {
+                        tracing_loki::layer(e, [].into_iter().collect(), [].into_iter().collect())
+                    })
+                    .transpose()
+                    .unwrap_or_else(|e| init_error!("Failed to initialize Loki exporter: {e}"))
+                    .unzip();
+
+                if let Some(loki_layer) = loki_layer {
+                    init_subscriber(log_filter, |r| r.with(loki_layer));
+                } else {
+                    init_subscriber(log_filter, |r| r);
+                }
+
+                std::mem::drop(span);
+
+                (common, loki_task)
             },
+        );
+
+        let (common, loki_task) = smuggled;
+
+        error_span!("run").in_scope(|| {
+            let (common, extra) = match Common::new(common, loki_task) {
+                Ok(t) => t,
+                Err(e) => {
+                    error!("Failed to initialize runtime: {e:?}");
+                    std::process::exit(-1);
+                },
+            };
+
+            std::process::exit(match main(common, extra) {
+                Ok(()) => 0,
+                Err(e) => {
+                    error!("{e:?}");
+                    1
+                },
+            });
         });
     }
 }
