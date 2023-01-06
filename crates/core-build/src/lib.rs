37a9b9cb16470f89df53784331eb8f2e668224de
raykast <me@ryan-s.net>
2023-01-06 13:54:33 -0800

    Avoid making schema GET requests for cached builds

--- crates/core-build/src/lib.rs
+++ crates/core-build/src/lib.rs
@@ -70,6 +70,62 @@ struct LockedSchema {
 
 type LockMap<'a> = HashMap<Cow<'a, str>, Cow<'a, LockedSchema>>;
 
+fn not_found_opt<T>(err: std::io::Error) -> Result<Option<T>, std::io::Error> {
+    if err.kind() == std::io::ErrorKind::NotFound {
+        Ok(None)
+    } else {
+        Err(err)
+    }
+}
+
+fn read_lock<'a>(
+    lock_map: &'a LockMap<'a>,
+    schema: &'a Schema,
+) -> Option<&'a Cow<'a, LockedSchema>> {
+    let locked = lock_map
+        .get(&Cow::Borrowed(&*schema.subject))
+        .and_then(|l| (l.schema == *schema).then_some(l));
+
+    locked
+}
+
+async fn check_schema(
+    path: impl AsRef<Path>,
+    schema: &Schema,
+    lock_map: &RwLock<LockMap<'_>>,
+) -> bool {
+    use sha2::Digest;
+    use tokio::io::AsyncReadExt;
+
+    let path = path.as_ref();
+    let Ok(mut file) = tokio::fs::File::open(path)
+        .await
+    else {
+        return false;
+    };
+
+    let mut buf = [0u8; 8192];
+    let mut digest = sha2::Sha512::default();
+
+    loop {
+        let n = match file.read(&mut buf).await {
+            Ok(0) => break,
+            Ok(n) => n,
+            Err(_) => return false,
+        };
+
+        if digest.write_all(&buf[0..n]).is_err() {
+            return false;
+        }
+    }
+
+    let sum = digest.finalize();
+    let lock_map_read = lock_map.read().await;
+    let locked = read_lock(&lock_map_read, schema);
+
+    locked.map_or(false, |l| l.sha512 == sum.as_slice())
+}
+
 async fn fetch_schema(
     out_dir: &Path,
     mut endpoint: url::Url,
@@ -79,6 +135,12 @@ async fn fetch_schema(
     use futures_util::StreamExt;
     use sha2::Digest;
     use tokio::io::AsyncWriteExt;
+
+    let path = out_dir.join(format!("{}.proto", schema.subject));
+
+    if check_schema(&path, &schema, lock_map).await {
+        return Ok(path);
+    }
 
     endpoint
         .path_segments_mut()
@@ -101,7 +163,6 @@ async fn fetch_schema(
         );
     }
 
-    let path = out_dir.join(format!("{}.proto", schema.subject));
     let mut outf = tokio::fs::File::create(&path)
         .await
         .with_context(|| format!("Failed to create {path:?}"))?;
@@ -120,9 +181,7 @@ async fn fetch_schema(
 
     let sum = digest.finalize();
     let lock_map_read = lock_map.read().await;
-    let locked = lock_map_read
-        .get(&Cow::Borrowed(&*schema.subject))
-        .and_then(|l| (l.schema == schema).then_some(l));
+    let locked = read_lock(&lock_map_read, &schema);
 
     if let Some(locked) = locked {
         anyhow::ensure!(
@@ -171,16 +230,8 @@ pub fn run(config_path: impl AsRef<Path>) -> Result<()> {
 
     let mut lock = String::new();
     std::fs::File::open(&lock_path)
-        .map_or_else(
-            |e| {
-                if e.kind() == std::io::ErrorKind::NotFound {
-                    Ok(None)
-                } else {
-                    Err(e)
-                }
-            },
-            |o| Ok(Some(o)),
-        )
+        .map(Some)
+        .or_else(not_found_opt)
         .with_context(|| format!("Failed to open {lock_path:?}"))?
         .map(|mut f| {
             f.read_to_string(&mut lock)
