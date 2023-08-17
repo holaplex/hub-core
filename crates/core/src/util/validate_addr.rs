95de388e081f71bbcae144168ccbf308695eb503
Abdul Basit <baronbasit@gmail.copm>
2023-08-17 16:21:30 +0500

    add fn is_blockchain_address

--- crates/core/src/util/validate_addr.rs
+++ crates/core/src/util/validate_addr.rs
@@ -1,20 +1,27 @@
-use std::str::FromStr;
-
 /// Trait containing functions to validate Blockchain address
 pub trait ValidateAddress {
-    /// Checks if it is an EVM address
-    fn is_evm_address(self) -> bool;
-
+    /// Checks if it is a valid Blockchain Address
     #[cfg(feature = "solana")]
+    fn is_blockchain_address(&self) -> bool;
+
+    /// Checks if it is an EVM address
+    fn is_evm_address(&self) -> bool;
+
     /// Checks if it is a Solana address
-    fn is_solana_address(self) -> bool;
+    #[cfg(feature = "solana")]
+    fn is_solana_address(&self) -> bool;
 }
 
 impl<S> ValidateAddress for S
 where
     S: AsRef<str>,
 {
-    fn is_evm_address(self) -> bool {
+    #[cfg(feature = "solana")]
+    fn is_blockchain_address(&self) -> bool {
+        self.is_evm_address() || self.is_solana_address()
+    }
+
+    fn is_evm_address(&self) -> bool {
         let address = self.as_ref();
         // EVM Address must start with 0x and have 42 characters
         if !address.starts_with("0x") || address.len() != 42 {
@@ -26,7 +33,9 @@ where
     }
 
     #[cfg(feature = "solana")]
-    fn is_solana_address(self) -> bool {
+    fn is_solana_address(&self) -> bool {
+        use std::str::FromStr;
+
         solana_sdk::pubkey::Pubkey::from_str(self.as_ref()).is_ok()
     }
 }
